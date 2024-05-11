3bc40439baa199682e09a7cba5794f43052c2f43
Rafael Ribeiro dos Santos <72527726+hjgms@users.noreply.github.com>
2024-05-11 11:36:53 -0300

    Update WebAPI.py

--- api/WebAPI.py
+++ api/WebAPI.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 from bs4 import BeautifulSoup
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 def scrape_banco(banco_nome):
     url = f'https://bancodata.com.br/relatorio/{banco_nome}/'
